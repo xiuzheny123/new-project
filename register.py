155dea707f3709bf0f5760ef96607e4a3b230820
xiuzhen yang <xiuzhenyangg@gmail.com>
2025-09-18 19:51:38 -0400

    register & login account

--- register.py
+++ register.py
@@ -1,18 +1,53 @@
-def register():
-    
-    username = input("Enter new username: ")
-    password = input("Enter new password: ")
+def main():
+    user_choice = input("Select:\n\t1 register \n\t2 login\n")
+    if user_choice == "1":
+        register()
+    elif user_choice == "2":
+        login()
 
-    with open("users.txt","r") as file:
-        for line in file:
-            stored_user,_ =line.strip().split(",")
-            if stored_user ==username:
-                print("Username already exists. Try another one")
-                return 
+
+def register():
+    while True:
+        username = input("Enter new username: ")
+        
+
+        with open("users.txt","r") as file:
+            taken = False
+            for line in file:
+                stored_user,_ =line.strip().split(",")
+                if stored_user ==username:
+                    print("Username already exists. Try another one")
+                    taken = True
+        if taken:
+            continue
+        password = input("Enter new password: ")
+
         with open("users.txt","a") as file:
             file.write(f"{username},{password}\n")
         print("Registration successful! please login now")
-        return
-register()
+        break
+def login():
+    while True:
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+        with open("users.txt","r") as file:
+            for line in file:
+                stored_user,stored_pass =line.strip().split(",")
+                if stored_user==username and stored_pass==stored_pass:
+                    print("Login successful! Welcome,", username)
+                    return
+        print("Invalid username or password")
+        
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
+
     
             
