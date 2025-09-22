54242975c568b0da9cc4709d3513f72d19604b73
xiuzhen yang <xiuzhenyangg@gmail.com>
2025-09-22 07:28:26 -0400

    class question

--- models.py
+++ models.py
@@ -9,13 +9,23 @@ class Question:
         self.answer = answer
 
     def ask(self,number=None):
-        pass
+        if number:
+            print(f"\nQ{number}: {self.text}")
+        else:
+            print(f"{self.qid}: {self.text}")
+
+        for i, opt in enumerate(self.options):
+            print(f"{chr(65+i)}){opt}")
+
+        return input("Your answer (A/B/C/D): ").strip().upper()
+
 
         # ask question here
 class Quiz:
     def __init__(self,file_path):
         self.questions_file = file_path
         self.questions = self.load()
+        
 class MistakeList:
     def __init__(self,file_path):
         self.file_path = file_path
