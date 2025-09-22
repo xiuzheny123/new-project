aa1ad2f1aa3ca6621cc0ba5a911726ee358e62e6
xiuzhen yang <xiuzhenyangg@gmail.com>
2025-09-22 14:33:32 -0400

    mistakelist

--- models.py
+++ models.py
@@ -25,11 +25,60 @@ class Quiz:
     def __init__(self,file_path):
         self.questions_file = file_path
         self.questions = self.load()
-        
+
+
+    def load_questions(self):
+        with open(self.questions_file,"r") as f:
+            data = json.load(f)
+        return [Question(q["id"]), q["question"], q["options"], q["answer"] for q in data]
+
+    def run(self ):
+        pass
+
+
 class MistakeList:
     def __init__(self,file_path):
         self.file_path = file_path
         self.mistakes = self.load()
+
+    def load(self):
+        try:
+            with open(self.file_path,"r") as f:
+                content = f.read().strip()
+                if not content:
+                    return []
+                return json.loads(content)
+        except FileNotFoundError:
+            return []
+    def save(self):
+        with open(self.file_path, "w") as f:
+            json.dump(self.mistakes,f, indent=2)
+
+    def add(self,question):
+        if not any(m["id"] == question.qid for m in self.mistakes):
+            self.mistakes.append({
+                "id": question.qid,
+                "question": question.text,
+                "options": question.options,
+                "answer": question.answer
+            })
+            self.save()
+
+    def remove(self,qid):
+        self.mistakes = [m for m in self.mistakes if m["id"] != qid]
+        self.save()
+    def contains(self,qid):
+        return any(m["id"] == qid for m in self.mistakes)
+    def get_all(self):
+        return self.mistakes
+    
+
+        
+
+
+
+
+
 class NoteManager:
     def __init__(self,file_path):
         self.file_path = file_path
