dd5550a17b8bd26763551486d609cef9d391f431
xiuzhen yang <xiuzhenyangg@gmail.com>
2025-09-23 20:33:31 -0400

    add an exception

---
+++ exceptions.py
@@ -0,0 +1,2 @@
+class InvalidMenuChoice(Exception):
+    pass
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from models import Quiz, MistakeList, NoteManager
+from exceptions import InvalidMenuChoice
 
 
 def main():
@@ -7,18 +8,21 @@ def main():
     notes = NoteManager("notes.json")
 
     while True:
-        user_choice = input("Select:\n\t1 Take full Quiz \n\t2 Take Mistake Quiz\n\t3 View Notes\n\t4 Quit\n")
-        if user_choice == "1":
-            quiz.run(mistakes,notes, mode="full")
-        elif user_choice == "2":
-            quiz.run(mistakes, notes,mode="mistake")
-        elif user_choice == "3":
-            notes.view_all()
-        elif user_choice == "4":
-            print("Goodbye")
-            break
-        else:
-            print("Invalid choice, please try again")
+        try:
+            user_choice = input("Select:\n\t1 Take full Quiz \n\t2 Take Mistake Quiz\n\t3 View Notes\n\t4 Quit\n")
+            if user_choice == "1":
+                quiz.run(mistakes,notes, mode="full")
+            elif user_choice == "2":
+                quiz.run(mistakes, notes,mode="mistake")
+            elif user_choice == "3":
+                notes.view_all()
+            elif user_choice == "4":
+                print("Goodbye")
+                break
+            else:
+                raise InvalidMenuChoice(f"{user_choice} is not a valid option, please choose 1-4")
+        except InvalidMenuChoice as e:
+            print(f"Error:{e}")
 
 
 
